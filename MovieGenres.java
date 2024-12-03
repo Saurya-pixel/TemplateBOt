b8249974c672398e0975b7e7628ea4c58ca13851
Saurya-pixel <sauri.bachala@gmail.com>
2024-12-03 04:48:38 +0000

    Some small changes to MovieGenres class prior to submission

--- MovieGenres.java
+++ MovieGenres.java
@@ -8,4 +8,33 @@ public class MovieGenres {
             new Movie("The Black Phone",
                     "After being abducted and locked in a basement, a boy starts receiving calls on a disconnected phone from the killer's previous victims.",
                     2021, "R") };
+
+        public static Movie[] Comedy = { new Movie("ted",
+        "John Bennett, a man whose childhood wish of bringing his teddy bear to life came true, now must decide between keeping the relationship with the bear, Ted or his girlfriend, Lori.",
+        2012, "R"),
+        new Movie("Shrek",
+                      "A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.",
+                      2001, "PG"),
+        new Movie("Friday",
+                      "It's Friday, and Craig and Smokey must come up with $200 they owe a local bully or there won't be a Saturday.",
+                      1995, "R") };
+        public static Movie[] Action = { new Movie("John Wick",
+        "John Wick, a former hitman is forced to return to action and seek revenge after his home is broken into and robbed, and his dog killed.",
+        2014, "R"),
+        new Movie("Interstellar",
+                      "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, are tasked with finding a new planet for humans.",
+                      2014, "PG-13"),
+        new Movie("The Hunger Games",
+                      "Katniss Everdeen is forced to participate in the Hunger Games: a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death.",
+                      2012, "PG-13") };
+        public static Movie[] Romantic = { new Movie("La La Land",
+        "When Sebastian, a pianist, and Mia, an actress, follow their passion and achieve success in their respective fields, they find themselves torn between their love for each other and their careers.",
+        2016, "PG-13"),
+        new Movie("Love Actually",
+                      "Love Actually follows the lives of eight very different couples in dealing with their love lives in various loosely interrelated tales all set during a frantic month before Christmas in London, England.",
+                      2003, "PG"),
+        new Movie("The Fault in Our Stars",
+                      "Two teenagers, Hazel and Gus, meet at a cancer support group and fall in love.",
+                      2014, "PG-13") };
+        
 }
